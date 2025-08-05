967c669740cbacf59b7dae6f6914355180c44e26
md1frejo <jonas.mb@gmail.com>
2025-08-05 15:33:16 +0200

    from terminal

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <p class="text-center text-lg text-headlinecard-600">copyright: jonas fredriksson</p>
+  <p class="text-center text-lg text-headlinecard-600">copyright: jf</p>
 </template>
 
 <style scoped>
